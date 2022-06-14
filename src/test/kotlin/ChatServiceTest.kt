017b08c6f2989dc2723aae86b67fbcc1345fc290
GalinaShiryaeva <Galla1777.35@gmail.com>
2022-06-14 12:54:59 +0300

    minor changes

--- src/test/kotlin/ChatServiceTest.kt
+++ src/test/kotlin/ChatServiceTest.kt
@@ -10,17 +10,17 @@ class ChatServiceTest {
     val Kate = User(3, "Kate")
     val Swen = User(4, "Swen")
 
-    val message11 = Message(11, Anna.id, Jack.id, 11, true, "Hey there!")
-    val message12 = Message(12, Anna.id, Jack.id, 11, true, "How are you?")
+    private val message11 = Message(11, Anna.id, Jack.id, 11, true, "Hey there!")
+    private val message12 = Message(12, Anna.id, Jack.id, 11, true, "How are you?")
 
-    val message21 = Message(21, Anna.id, Kate.id, 12, true, "Hey Kate!")
-    val message22 = Message(22, Kate.id, Anna.id, 12, false, "Hi!")
-    val message23 = Message(23, Kate.id, Anna.id, 12, true, "Hi again!")
+    private val message21 = Message(21, Anna.id, Kate.id, 12, true, "Hey Kate!")
+    private val message22 = Message(22, Kate.id, Anna.id, 12, false, "Hi!")
+    private val message23 = Message(23, Kate.id, Anna.id, 12, true, "Hi again!")
 
-    val message31 = Message(31, Jack.id, Kate.id, 13, true, "Hi!")
-    val message32 = Message(32, Jack.id, Kate.id, 13, false, "Hello")
+    private val message31 = Message(31, Jack.id, Kate.id, 13, true, "Hi!")
+    private val message32 = Message(32, Jack.id, Kate.id, 13, false, "Hello")
 
-    val message41 = Message(41, Swen.id, Jack.id, 14, false, "Hello")
+    private val message41 = Message(41, Swen.id, Jack.id, 14, false, "Hello")
 
     init {
         ChatService.sendMessage(message11)
@@ -69,38 +69,41 @@ class ChatServiceTest {
     @Test
     fun sendMessage() {
         // arrange
-        val chat = ChatService.getChatById(message11.chatId) ?: error("Chat not found")
+        val message = message11
+        val chat = ChatService.getChatById(message.chatId) ?: error("Chat not found")
 
         // act
-        val result = ChatService.sendMessage(message11)
+        val result = ChatService.sendMessage(message)
 
         // assert
-        assertTrue("Message is not existing", chat.messages.contains(message11))
-        assertTrue("Chat is not existing", ChatService.chats.any { it.id == message11.chatId })
-        assertTrue("This message has a duplicate", chat.messages.count { it.id == message11.id } == 1 )
+        assertTrue("Message is not existing", chat.messages.contains(message))
+        assertTrue("Chat is not existing", ChatService.chats.any { it.id == message.chatId })
+        assertTrue("This message has a duplicate", chat.messages.count { it.id == message.id } == 1 )
     }
 
     @Test
     fun deleteMessage() {
         // arrange
-        val chat = ChatService.getChatById(message41.chatId) ?: error("Chat not found")
+        val message = message41
+        val chat = ChatService.getChatById(message.chatId) ?: error("Chat not found")
 
         // act
-        val result = ChatService.deleteMessage(message41)
+        val result = ChatService.deleteMessage(message)
 
         // assert
         assertTrue("Failed to delete message", result)
-        assertTrue("Deleted message ID is detected", chat.messages.none { it.id == message41.id })
-        assertTrue("Empty chat is not deleted", ChatService.chats.none { it.id == message41.chatId })
+        assertTrue("Deleted message ID is detected", chat.messages.none { it.id == message.id })
+        assertTrue("Empty chat is not deleted", ChatService.chats.none { it.id == message.chatId })
     }
 
     @Test
     fun deleteChat() {
         // arrange
-        val chat = ChatService.chats.find { it.id == message32.chatId } ?: error("Chat not found")
+        val message = message32
+        val chat = ChatService.chats.find { it.id == message.chatId } ?: error("Chat not found")
 
         // act
-        val result = ChatService.deleteChat(message32.chatId)
+        val result = ChatService.deleteChat(message.chatId)
 
         // assert
         assertTrue("Failed to delete chat", result)
@@ -110,10 +113,11 @@ class ChatServiceTest {
     @Test
     fun edit() {
         // arrange
+        val message = message22
         val newText = "New text for message edit"
 
         // act
-        val result = ChatService.edit(message22, newText)
+        val result = ChatService.edit(message, newText)
 
         // assert
         assertTrue("Failed to edit message", result.text == newText)
@@ -121,11 +125,14 @@ class ChatServiceTest {
 
     @Test
     fun getChatById() {
+        // arrange
+        val message = message31
+
         // act
-        val result = ChatService.getChatById(message31.chatId)
+        val result = ChatService.getChatById(message.chatId)
 
         // assert
-        assertTrue("Chat not found", result.id == message31.chatId)
+        assertTrue("Chat not found", result.id == message.chatId)
     }
 
     @Test
